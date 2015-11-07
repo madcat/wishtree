0595abc60d5493c93e2806a62aaf59dc6d50e7e6
LF <lfsong@gmail.com>
2015-11-07 19:51:27 +0800

    add photowall

---
+++ public/js/photowall.js
@@ -0,0 +1,24 @@
+var grid = {}
+grid.controller = function(){
+  var c = this
+  c.guests = m.prop([])
+  c.getGuests = function(){
+    m.request({method:"GET", url:"/wishes"}).then(function(data){
+      c.guests(data.body)
+    })
+  }
+  c.getGuests()
+}
+grid.view = function(ctrl){
+  return m(".container", [
+    m(".grid", [
+      ctrl.guests().map(function(g){
+        return m(".cell-box", [
+          m("img.cell.cell-on", {id:"g"+g.id, src:"/"+g.pic_path})
+        ])
+      })
+    ])
+  ])
+}
+
+m.mount(document.getElementById("content"), grid);
